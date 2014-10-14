6c212ac09966a3eef9cd3d432e46eff36cdff016
hw5000 <>
2014-10-14 11:27:27 -0400

    Updated Copyright

--- PaintApp/src/paintapp/ColorButton.java
+++ PaintApp/src/paintapp/ColorButton.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2014 Aldrich.
+ * Copyright 2014 Aldrich Fung, Hermes Frangoudis (hwf5000).
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
